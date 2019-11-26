2b98fe0009f936b361e3b90f783c12b3e742e68a
Кирилл <tkirill42@mail.ru>
2019-11-26 23:51:24 +0700

    5.4

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class TestOne {
+    private $name;
+    private $age;
+    private $isBoy;
+
+    public function get() {
+        return ["name"=>$this->name, "age"=>$this->age, "isBoy"=>$this->isBoy]  ;
+    }
+
+}
+
 class TestController extends Controller
 {
+    public function getOne() {
+    $test = new TestOne();
+    return $test->get();
+    }
     //
 }
